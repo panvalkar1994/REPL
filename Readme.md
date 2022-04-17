556271e61cc9fd7dfe9842e67b8e7c965fa9bc11
Vaibhav Panvalkar <vaibhav.panvalkar@elastic.run>
2022-04-17 21:08:09 +0530

    Polish Notation Parser

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# REPL
+
+## Special Thanks:
+
+[Daniel Holden](https://github.com/orangeduck) for his [mpc](https://github.com/orangeduck/mpc) which has great implementation for Micro Parser Combinator in C.
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include "mpc.h"
 #define true 1
 
 // Define version of the program
@@ -9,6 +10,17 @@ const int input_size = 1024;
 static char input[input_size];
 
 int main(int argc, char* argv[]) {
+
+    // Create some parser
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* REPL = mpc_new("repl");
+
+    // define language
+    mpca_lang(MPCA_LANG_DEFAULT,"number : /-?[0-9]+/; operator : '+' | '-' | '*' | '/'; expr : <number> | '('<operator> <expr>+ ')'; repl : /^/ <operator> <expr>+ /$/ ;",  Number, Operator, Expr, REPL);
+
+    mpc_result_t r;
     printf("Welcome to REPL %s\n", version);
     puts("Press Ctrl+c to Exit");
     while (true)
@@ -19,9 +31,22 @@ int main(int argc, char* argv[]) {
         // read input
         fgets(input,input_size, stdin);
 
-        // print 
-        printf("%s", input);
+        if (mpc_parse("<stdin>", input, REPL, &r))
+        {
+            // Success print
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        }
+        else
+        {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+                
     }
+
+    // cleanup parser
+    mpc_cleanup(4,Number,Operator,Expr,REPL);
     
     return 0;
 }

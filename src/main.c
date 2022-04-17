bda7fb966e9a9afab683d287195e079cf54669b6
Vaibhav Panvalkar <vaibhav.panvalkar@elastic.run>
2022-04-17 14:01:52 +0530

    basic read and print

--- src/main.c
+++ src/main.c
@@ -1,6 +1,27 @@
 #include<stdio.h>
+#define true 1
+
+// Define version of the program
+const char* version = "v0.0.0.1";
+
+// declare a bufer for input
+const int input_size = 1024;
+static char input[input_size];
 
 int main(int argc, char* argv[]) {
-    printf("Hello World!");
+    printf("Welcome to REPL %s\n", version);
+    puts("Press Ctrl+c to Exit");
+    while (true)
+    {
+        // Prompt
+        fputs("REPL> ", stdout);
+
+        // read input
+        fgets(input,input_size, stdin);
+
+        // print 
+        printf("%s", input);
+    }
+    
     return 0;
 }
